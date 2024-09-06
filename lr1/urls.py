03aea410e2990fb53a3ed81b77c8f9f7d8ff9360
resxton <gornomirov661@gmail.com>
2024-09-06 13:50:08 +0300

    django started

---
+++ lr1/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from lr1_code import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello/', views.hello),
+]
---
+++ lr1_code/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Lr1CodeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lr1_code'
