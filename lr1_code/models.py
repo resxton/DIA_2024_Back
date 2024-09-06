afda0388b1923900dd65f2b2f099c24d1b0afc20
resxton <gornomirov661@gmail.com>
2024-09-06 14:48:24 +0300

    gitignore

--- lr1_code/models.py
+++ lr1_code/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Airplane(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    passenger_count = models.IntegerField()
+    range = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
